6cbb4159c1aa9fd59051bea056255d57163e269f
sgdvesus <jesusvlzez@gmail.com>
2022-03-08 04:44:06 -0600

    feat: variables

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World, Alexander");
+        int currentBalance = 2400;
+        int TAX = 16;
+        //Java 10 ^
+        // var result = currentBalance*0.16;
+
+        String username = "Alexander";
+        System.out.println(currentBalance);
+
+        currentBalance -= 400;
+        System.out.println(username);
+        System.out.println(currentBalance);
+    }
+}
