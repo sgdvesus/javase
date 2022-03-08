f2ce0a3f800222607082cc4a8d8a002ed57a1db3
sgdvesus <jesusvlzez@gmail.com>
2022-03-08 06:58:31 -0600

    feat: data types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,16 +1,34 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World, Alexander");
-        int currentBalance = 2400;
-        int TAX = 16;
-        //Java 10 ^
-        // var result = currentBalance*0.16;
-
-        String username = "Alexander";
-        System.out.println(currentBalance);
-
-        currentBalance -= 400;
+        //strings
+        String username = "Alexander ";
+        username += "Salgado ";
+        username += "Velazquez";
         System.out.println(username);
-        System.out.println(currentBalance);
+        //char
+        char myChar = 'z';
+        System.out.println("For char: " + myChar);
+
+        //bool
+        boolean premiumAccount = true;
+        boolean freeAccount = false;
+        System.out.println("is premium: " + premiumAccount + ". Is free: " + freeAccount);
+
+        //integers
+        byte age = 127;
+        short limitBalance  = 32767;
+        int currentBalance = 2400; //default
+        long bankBalance = 9223372036854775807L;
+        System.out.println("Age: " + age + " limit user: " + limitBalance);
+        System.out.println("Current user balance: " + currentBalance + "bank: " + bankBalance);
+
+        //float
+        double commision = 2.9d;
+        float clientDeposit = 19.8F;
+        System.out.println("Bank commision: " + commision + "clientDeposit " + clientDeposit);
+
+
+        //for Java 10 ^ can use "var" keyword
+        // var result = currentBalance*0.16
     }
 }
